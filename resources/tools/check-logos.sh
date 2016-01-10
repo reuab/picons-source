9d9c453c7b33eb814c0ea60d8787e50695122480
mocram <inh2ru9287+mocram@gmail.com>
2016-01-10 21:42:29 +0100

    fix and speed up checking of logos

--- resources/tools/check-logos.sh
+++ resources/tools/check-logos.sh
@@ -6,19 +6,14 @@ for file in * ; do
     if [[ ! -f ${file%.*.*}.default.png ]] && [[ ! -f ${file%.*.*}.default.svg ]]; then
         echo The following logo has no default version: $file
     fi
-done
-
-for file in *.default.svg ; do
-    if [[ -f ${file%.*.*}.*.png ]]; then
-        echo The following default logo is an svg, but has one or more png alternatives: $file
+    
+    if [[ $file == *.svg && ( -f ${file%.*.*}.default.png || -f ${file%.*.*}.light.png || -f ${file%.*.*}.dark.png || -f ${file%.*.*}.black.png || -f ${file%.*.*}.white.png ) ]]; then
+        echo The following logo is an svg, but has one or more png alternatives: $file
     fi
-done
-
-for file in *.svg ; do
-    if grep -q "</text>" $file; then
-        echo This svg contains text: $file
-    fi
-    if grep -q "data:image/png" $file; then
-        echo This svg contains a png image: $file
+    
+    if [[ $file == *.svg ]]; then
+        if grep -q -e "</text>" -e "data:image/png" $file; then
+            echo This svg contains text or a png image: $file
+        fi
     fi
 done
